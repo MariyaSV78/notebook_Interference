e2d796250bdd4ecab67f04d3fbb6400413285d84
Mariya <mariy.v.sosnova@gmail.com>
2024-11-26 17:45:59 +0100

    gravimetre

--- gravity_sparce.py
+++ gravity_sparce.py
@@ -8,7 +8,7 @@ from matplotlib.animation import FuncAnimation, FFMpegWriter
 
 """Système d'unité"""
 hbar = 1  # Constante de Planck réduite
-mu = 0.5  # Masse de l'électron
+mu = 1  # Masse de l'électron
 metre = 6.67e-9  # en unités naturelles (=Longueur d'un mètre dans notre système d'unité)
 g = 6.67e-24  # en unités naturelles (= valeur de g dans notre système d'unité)
 
@@ -19,7 +19,7 @@ def Kinetic1D(N, Dx, mu=1):
     main_diag = np.full(N, -2 * rx)  # Main diagonal (central points)
     off_diag = np.full(N - 1, rx)  # Off-diagonal (neighboring points)
 
-    # Create a sparse 1D Laplacian
+    # Create a sparse 1D derivative
     return sp.diags([off_diag, main_diag, off_diag], [-1, 0, 1], shape=(N, N))
 
 
@@ -35,7 +35,7 @@ def Hamiltonian2D(Dx, Dy, V, mu=1):
 
 
 # Fonction principale, simulation dans boîte carrée
-def simulation(psi0, V, Dx, Dy, Nt=200, mu=1):
+def simulation(psi0, V, Dx, Dy, Nt=200, mu=1, skip=5):
     """
     psi0: fonction d'onde initiale, taille Lx*Ly, (Nx-2)*(Ny-2) points
     V : potentiel, taille (NxN) points
@@ -47,8 +47,7 @@ def simulation(psi0, V, Dx, Dy, Nt=200, mu=1):
     Nx, Ny = V.shape
 
     # Résolution du sysème Ax=My à chaque itération
-    density = []  # Densité de proba à chaque itération
-    density.append(np.abs(psi0) ** 2)  # Stockage de density0 dans la liste
+    density = [np.abs(psi0) ** 2]  # Stockage de density0 dans la liste
 
     # Résolution via Cranck Nichoslon
     Dt = (Dx/2)**2 + (Dy/2)**2
@@ -57,7 +56,7 @@ def simulation(psi0, V, Dx, Dy, Nt=200, mu=1):
 
     psi_vect = psi0.flatten()
     Energy0 = np.real(np.conj(psi_vect) @ H @ psi_vect) / np.real(np.conj(psi_vect) @ psi_vect)
-    print(f"Initial WP energy: {Energy0:.5f} (potential min/max {np.min(V):.3f}/{np.max(V):.3f})")
+    print(f"Initial WP energy: {Energy0:.5f} (potential min/max {np.min(V):.2f}/{np.max(V):.2f})")
 
     E = sp.eye(H.shape[0], dtype=H.dtype)
     Asp, Bsp = E + 1j * Dt / 2 * H, E - 1j * Dt / 2 * H
@@ -65,7 +64,7 @@ def simulation(psi0, V, Dx, Dy, Nt=200, mu=1):
     for i in range(1, Nt+1):
         psi_vect = lsp.spsolve(Asp, Bsp @ psi_vect)
 
-        if i % 5 == 0:
+        if i % skip == 0:
             print(f"{i} / {Nt}")  # Pour voir la progression
             # Ajout aux listes
             psi2 = np.abs(psi_vect.reshape((Nx, Ny)))**2
@@ -82,8 +81,16 @@ Dx = 0.03
 Dy = 0.03
 Nx = int(Lx / Dx) + 1
 Ny = int(Ly / Dy) + 1
+Lx = Dx * Nx
+Ly = Dy * Ny
 # Nt = 1500
-Nt = 600
+dt = 0.01
+Nt = 50
+skip = int(dt/((Dx / 2)**2 + (Dy / 2)**2))  # save data after "skip" time points (skip = 1 - save all time points)
+t_max = Nt * dt
+Nt0 = Nt*skip
+
+
 
 """Fonction d'onde initiale"""
 sigmax = 0.4  # Largeur du paquet d'onde initial
@@ -101,16 +108,18 @@ V = g * Y
 
 print(f"V shape = {V.shape}")
 Energy0 = 1/(2*mu) * (1/sigmax**2 + 1/sigmay**2)/2 + g * y0
-print(f"Initial WP energy (analytical): {Energy0:.5f} (potential min/max {np.min(V):.3f}/{np.max(V):.3f})")
+print(f"Initial WP energy (analytical): {Energy0:.5f} (potential min/max {np.min(V):.2f}/{np.max(V):.2f})")
 
 """Simulation"""
 psi0 = np.exp(-1 / 2 * (((X - x0) / sigmax)**2 + ((Y - y0) / sigmay)**2))  # Fonction d'onde initiale
-density = simulation(psi0, V, Dx, Dx, Nt, mu)
+psi0 /= np.sqrt(np.conj(psi0) @ psi0)
+density = simulation(psi0, V, Dx, Dx, Nt0, mu, skip=skip)
 
 
 
 """Visualisation de la fonction d'onde initiale et du potentiel"""
-fig, ax = plt.subplots(1, 2, figsize=(14, 6))
+fig = plt.figure(figsize=(14, 6))
+ax = (fig.add_subplot(121), fig.add_subplot(122, projection='3d'))  # Important: projection='3d'
 
 # Densité de probabilité
 cax1 = ax[0].imshow(density[0], extent=(0, Lx, 0, Ly), origin='lower', cmap='magma')
@@ -120,7 +129,10 @@ ax[0].set_ylabel('y')
 fig.colorbar(cax1, ax=ax[0])
 
 # Potentiel
-cax2 = ax[1].imshow(V, extent=(0, Lx, 0, Ly), origin='lower', cmap='binary')
+# cax2 = ax[1].imshow(V, extent=(0, Lx, 0, Ly), origin='lower', cmap='binary')
+Norm = 0.1 * (np.max(V) - np.min(V)) / np.max(density[0])
+cax2 = ax[1].plot_surface(X, Y, V, cmap='viridis', edgecolor='none')
+cax3 = ax[1].plot_surface(X, Y, Energy0 + Norm * density[0], cmap='viridis', edgecolor='none')
 ax[1].set_title('Potentiel')
 ax[1].set_xlabel('x')
 ax[1].set_ylabel('y')
@@ -141,6 +153,8 @@ animation_is_playing = True
 # ax = fig.add_subplot(111, xlim=(0, Lx), ylim=(0, Ly))
 # img = ax.imshow(density[0], extent=[0, Lx, 0, Ly], origin='lower', cmap="magma", vmin=0, zorder=1)  # Here the modulus of the 2D wave function shall be represented.
 img = cax1
+img2 = cax3
+
 # Fonction pour l'animation
 def animate(i):
     slider.set_val(i)  # Update slider position to match the current frame
@@ -160,11 +174,16 @@ def toggle_play(event):
 
 # Function to update frame with slider
 def slider_update(val):
-    global pause, anim, animation_is_playing
+    global pause, anim, img2, animation_is_playing
     i = int(val)
-    img.set_data(density[i])
+    Z = density[i]
+    img.set_data(Z)
     img.set_zorder(1)
-    img.set_clim(vmin=0, vmax=np.max(density[i]))
+    img.set_clim(vmin=0, vmax=np.max(Z))
+
+    img2.remove()  # Remove the old surface
+    img2 = ax[1].plot_surface(X, Y, Energy0 + Norm * Z, cmap='viridis', edgecolor='none')
+
     fig.canvas.draw_idle()
 
 pos = ax[0].get_position()
@@ -185,4 +204,3 @@ writer = FFMpegWriter(fps=30, metadata=dict(artist='Me'), bitrate=1800)
 anim.save("pesanteur_electron100.mp4", writer=writer)
 
 plt.show()
-
